1db53968bd9f1d432a4b2d840f1078e6a8e92c67
Farhan Fiaz Alvi <farhanalvi005@gmail.com>
2024-03-08 15:53:51 +0600

    forgot to update help string

--- main.go
+++ main.go
@@ -15,9 +15,10 @@ const help_str = `
 hpoon: Harpoon for the shell
 
 Usage:
-    hpoon [dir] [opt-name]  | store a mark, optionally with a name
-    hpoon                   | retreive the last marked file or dir
-    hpoon @[name]           | retreive marked file with name
+    hpoon <path> [name]         | store a mark, optionally with a name
+    hpoon                       | retrieve the last marked path
+    hpoon !<name>               | retrieve marked path with name (mark is recognized by prefix "!")
+    hpoon clean                 | delete all hpoon history
 
     can only mark files and directories that exist, but can retreive
     marks that no longer exist on the filesystem
@@ -29,13 +30,13 @@ Examples:
 
     # in a different shell (ie: tmux)
     cd /new/abs/dir     # totally different dir
-    cp * ` + "`hpoon`" + `      # copy files over to the last harpooned dir   
-    
+    cp * ` + "`hpoon`" + `      # copy files over to the last harpooned dir
+
     # works on deleted files
-    hpoon filename myfile           #harpoon a file with "myfile"
+    hpoon filename myfile   #harpoon a file with "myfile"
     rm filename
     cd /somewhere/else/entirely
-    mv that_file ` + "`hpoon @myfile`" + `  # result: mv that_file /original/path/filename
+    mv some_file ` + "`hpoon !myfile`" + `   # result: mv some_file /original/path/filename
 `
 
 const short_help = "Unsure arg, try -h to get usage information"
